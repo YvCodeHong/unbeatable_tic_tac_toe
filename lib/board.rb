ee83405562c4421117a773b959b83902f528f27c
Dan Pelensky <dan@pelensky.com>
2017-02-04 15:28:52 +0000

    user can not place their marker somewhere where a space has been taken

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,21 @@
 class Board
 
-attr_reader :spaces
+  attr_reader :spaces
 
-def initialize
-@spaces = [0,1,2,3,4,5,6,7,8]
+  def initialize
+    @spaces = [0,1,2,3,4,5,6,7,8]
+  end
+
+  def take_turn(space, player)
+    return "Illegal move" if space_taken(space)
+    @spaces[space] = player
+  end
+
+
+private
+
+def space_taken(space)
+  @spaces[space] == ("X" || "O")
 end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,9 +3,21 @@ require 'board'
 describe Board do
   subject(:board) { described_class.new}
 
-  context "#initialize" do
+  context "The board" do
     it "should initialize with an array numbered 0 - 8" do
       expect(board.spaces).to eq([0,1,2,3,4,5,6,7,8])
+    end
+  end
+
+  context "When taking a turn it" do
+    it "should allow the user to place their marker in any empty space" do
+      board.take_turn(0, "X")
+      expect(board.spaces).to eq(["X",1,2,3,4,5,6,7,8])
+    end
+
+    it "should not allow the user to place their marker somewhere that has been taken" do
+      board.take_turn(0, "X")
+      expect(board.take_turn(0, "O")).to eq "Illegal move"
     end
   end
 end
