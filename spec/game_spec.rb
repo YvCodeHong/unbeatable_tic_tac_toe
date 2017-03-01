8f869eeb91033721524ffd49a8fa54e51604becf
Dan PElensky <dan@pelensky.com>
2017-03-01 08:47:56 +0000

    cleaned up game test names

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,32 +11,34 @@ describe Game do
   subject(:game) { described_class.new(player1, player2, board)}
 
   context "#initialize" do
-    it "should take two players and a board" do
+    it "takes two players and a board" do
       expect(game.player1).to eq player1
+      expect(game.player2).to eq player2
+      expect(game.board).to eq board
     end
   end
 
   context "#new_game" do
-    it "should always start with X" do
+    it "always starts the game with X" do
       game.new_game
       expect(game.current_player).to eq player1
     end
   end
 
-  context "playing the game" do
-    it "should allow the current player to place their move" do
+  context "#play" do
+    it "allows the current player to make their move" do
       game.new_game
       game.play(0)
       expect(game.show_board).to eq ["X",1,2,3,4,5,6,7,8]
     end
 
-    it "should change the current player once a move is made" do
+    it "changes the current player once a valid move is made" do
       game.new_game
       game.play(0)
       expect(game.current_player).to eq player2
     end
 
-    it "should not change the current player if their move was illegal" do
+    it "does not change the current player if their move was illegal" do
       game.new_game
       game.play(10)
       expect(game.current_player).to eq player1
@@ -44,7 +46,7 @@ describe Game do
 
   end
 
-  context "game over" do
+  context "end of game" do
 
     it "knows when x wins" do
       x_wins_two_player
@@ -58,12 +60,10 @@ describe Game do
       expect(game.winner).to eq player2.marker
     end
 
-    it "knows that nobody wins a tied game" do
+    it "knows when the game is tied" do
       tied_game_two_player
       game.update_game_status
       expect(game.winner).to eq nil
     end
-
   end
-
 end
