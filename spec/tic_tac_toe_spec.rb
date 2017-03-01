bf31e040fec78336cabba74fd0c2a0aa56fe2ad7
Dan PElensky <dan@pelensky.com>
2017-03-01 08:59:18 +0000

    cleaned up tic_tac_toe test names

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -8,32 +8,32 @@ describe TicTacToe do
   let(:game) { Game.new(player1, player2, board)}
 
   context "#welcome" do
-    it "should output a welcome when the game is run" do
+    it "outputs a welcome when the game is run" do
       expected_output = "============================\nLet's play Tic Tac Toe!\n     0 | 1 | 2\n   -------------\n     3 | 4 | 5\n   -------------\n     6 | 7 | 8\nSelect 1 for a two player game\nSelect 2 to play against the computer\nSelect 3 for two computers to play against each other\n"
       expect { tic_tac_toe.welcome }.to output(expected_output).to_stdout
     end
   end
 
   context "#select_game_type" do
-    it "invalid number" do
+    it "responds when there is an invalid number" do
       allow(tic_tac_toe).to receive(:gets).and_return('42')
       allow(tic_tac_toe).to receive(:loop).and_yield
       expect { tic_tac_toe.select_game_type }.to output("I didn't quite get that - 1, 2 or 3\n").to_stdout
     end
 
-    it "1 - Two players" do
+    it "handles 1 - Two players" do
       allow(tic_tac_toe).to receive(:gets).and_return('1')
       allow(tic_tac_toe).to receive(:two_player_game)
       expect { tic_tac_toe.select_game_type }.to output("Two players? Great choice\n").to_stdout
     end
 
-    it "2 - Player vs Computer" do
+    it "handles 2 - Player vs Computer" do
       allow(tic_tac_toe).to receive(:gets).and_return('2')
       allow(tic_tac_toe).to receive(:who_goes_first_prompt)
       expect { tic_tac_toe.select_game_type }.to output("Against the computer? You won't win!\n").to_stdout
     end
 
-    it "3 - Computer vs Computer" do
+    it "handles 3 - Computer vs Computer" do
       allow(tic_tac_toe).to receive(:gets).and_return('3')
       allow(tic_tac_toe).to receive(:computer_vs_computer)
       expect { tic_tac_toe.select_game_type }.to output("Watching two computers battle it out? Nice!\n").to_stdout
@@ -41,7 +41,7 @@ describe TicTacToe do
   end
 
   context "#two_player_game" do
-    it "creates a game with two player objects" do
+    it "creates a game with two players" do
       allow(tic_tac_toe).to receive(:who_goes_first_prompt)
       tic_tac_toe.two_player_game
       expect(tic_tac_toe.game.player1).to be_a_kind_of(Player)
@@ -50,7 +50,7 @@ describe TicTacToe do
   end
 
   context "#player_vs_computer" do
-    it "creates a game with two player objects" do
+    it "creates a game with a player and a computer" do
       allow(tic_tac_toe).to receive(:who_goes_first_prompt)
       tic_tac_toe.player_vs_computer_player_first
       expect(tic_tac_toe.game.player1).to be_a_kind_of(Player)
@@ -59,7 +59,7 @@ describe TicTacToe do
   end
 
   context "#computer_vs_computer" do
-    it "creates a game with two player objects" do
+    it "creates a game with two computers" do
       allow(tic_tac_toe).to receive(:who_goes_first_prompt)
       tic_tac_toe.computer_vs_computer
       expect(tic_tac_toe.game.player1).to be_a_kind_of(Computer)
@@ -75,20 +75,20 @@ describe TicTacToe do
   end
 
   context "#who_goes_first" do
-    it "invalid selection" do
+    it "hanles an invalid selection" do
       allow(tic_tac_toe).to receive(:gets).and_return('hello')
       allow(tic_tac_toe).to receive(:loop).and_yield
       expect { tic_tac_toe.who_goes_first }.to output("I didn't quite get that - 1 or 2?\n").to_stdout
     end
 
-    it "Player" do
+    it "hanles a player" do
       allow(tic_tac_toe).to receive(:gets).and_return('1')
       allow(tic_tac_toe).to receive(:create_new_game)
       allow(tic_tac_toe).to receive(:player_vs_computer_player_first)
       expect { tic_tac_toe.who_goes_first }.to output("You go first!\n").to_stdout
     end
 
-    it "Computer" do
+    it "handles a computer" do
       allow(tic_tac_toe).to receive(:gets).and_return('2')
       allow(tic_tac_toe).to receive(:create_new_game)
       allow(tic_tac_toe).to receive(:player_vs_computer_computer_first)
@@ -98,7 +98,7 @@ describe TicTacToe do
 
 
   context "#player_turn" do
-    it "Invalid - text" do
+    it "handles invalid text" do
       allow(tic_tac_toe).to receive(:who_goes_first_prompt)
       tic_tac_toe.two_player_game
       tic_tac_toe.game.new_game
@@ -108,7 +108,7 @@ describe TicTacToe do
       expect { tic_tac_toe.player_turn }.to output("Not a number. Try again!\n").to_stdout
     end
 
-    it "Valid cell" do
+    it "handles a valid cell" do
       allow(tic_tac_toe).to receive(:who_goes_first_prompt)
       tic_tac_toe.two_player_game
       tic_tac_toe.game.new_game
@@ -118,7 +118,7 @@ describe TicTacToe do
       expect { tic_tac_toe.player_turn }.to output("     X | 1 | 2\n   -------------\n     3 | 4 | 5\n   -------------\n     6 | 7 | 8\n").to_stdout
     end
 
-    it "Taken cell" do
+    it "handles a taken cell" do
       allow(tic_tac_toe).to receive(:who_goes_first_prompt)
       tic_tac_toe.two_player_game
       tic_tac_toe.game.new_game
